1c43e33f98c628532eb389ff7088f1f68f6d6ac8
Wilfried Klaebe <wklaebe@users.noreply.github.com>
2014-02-24 23:29:03 +0000

    re-add GPL notice which I accidentially deleted add GPL notice to core_channel.py small cleanups

--- scripts/core_channel.py
+++ scripts/core_channel.py
@@ -1,8 +1,22 @@
+# This file is part of Piracast.
+#
+#     Piracast is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Piracast is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Piracast.  If not, see <http://www.gnu.org/licenses/>.
 
 import socket
 import sys
 
-def end() : 
+def end():
 
     # Create a UDS socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -20,5 +34,5 @@ def end() :
     print >>sys.stderr, 'sending "%s"' % message
     sock.sendall(message)
 
-    sock.close(); 
+    sock.close()
 
--- scripts/sink.py
+++ scripts/sink.py
@@ -12,7 +12,6 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with Piracast.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 import re
 import socket
